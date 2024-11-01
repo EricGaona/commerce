58d785ccda46b74dd30672e18440dc683a2979db
eric <ericgaona0413@gmail.com>
2024-11-01 16:06:09 +0000

    Adding code to the urls.py and models.py file.

--- auctions/urls.py
+++ auctions/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("create", views.create_listing, name="create_listing"),
+    path("listing/<int:listing_id>", views.listing_page, name="listing_page"),
+    path("watchlist", views.watchlist, name="watchlist"),
+    path("categories", views.categories, name="categories"),
+    path("category/<str:category_name>", views.category_listings, name="category_listings"),
 ]
